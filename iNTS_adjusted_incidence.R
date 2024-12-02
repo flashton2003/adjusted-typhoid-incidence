6293b82a26424bf11f1627ed1f3655515cb63561
flashton2003 <flashton@gmail.com>
2024-12-02 14:45:28 +0200

    added mitima

--- iNTS_adjusted_incidence.R
+++ iNTS_adjusted_incidence.R
@@ -15,19 +15,59 @@ set.seed(012)
 #### DATA LOADING #######
 #########################
 # Load data
-blood_culture_data <- read_csv("~/Dropbox/GordonGroup/iNTS_Typhimurium_updates/data_munging/2024.12.02/2024.12.02.strataa_data_for_model.csv", col_types = cols(date = col_date(format = "%Y-%m-%dT%H:%M:%SZ")))
+strataa_blood_culture_data <- read_csv("~/Dropbox/GordonGroup/iNTS_Typhimurium_updates/data_munging/2024.12.02/2024.12.02.strataa_data_for_model.csv", col_types = cols(date = col_date(format = "%Y-%m-%dT%H:%M:%SZ")))
 
-blood_culture_data <- blood_culture_data %>%
+strataa_blood_culture_data <- strataa_blood_culture_data %>%
   filter(date > ymd("2016-12-31")) %>%
   filter(date < ymd('2020-01-01'))
 
+mitima_data <- read.csv("/Users/flashton/Dropbox/GordonGroup/iNTS_Typhimurium_updates/data_munging/2024.12.02/2024.12.02.mitima_data_for_model.csv", colClasses = c(DSP = "character")) %>%
+  mutate(date = as.Date(DSP, format = "%d-%b-%Y")) %>% 
+  select (date, bc_positive) %>% 
+  mutate(bc_tested = 1) # all these have had bc_tested
+
+add_random_dates <- function(df) {
+  # Get start and end dates from original data
+  start_date <- min(df$date)
+  end_date <- max(df$date)
+  
+  # Calculate number of rows to add (25% of original)
+  n_new_rows <- ceiling(nrow(df) * 0.25)
+  
+  # Generate random dates between start and end
+  random_dates <- sample(
+    seq(start_date, end_date, by = "day"),
+    size = n_new_rows,
+    replace = TRUE
+  )
+  
+  # Create new dataframe with random dates
+  new_rows <- data.frame(
+    date = random_dates,
+    bc_tested = 0,
+    bc_positive = 0
+  )
+  
+  # Combine with original dataframe and sort by date
+  combined_df <- rbind(df, new_rows) %>%
+    arrange(date)
+  
+  return(combined_df)
+}
+
+# need to add participants who didn't receive blood culture, overall, was around 20% of samples, so adding 25% (25% of 80% brings us up oto 100% of original)
+mitima_data <- add_random_dates(mitima_data)
+mitima_data <- mitima_data %>% select(date, bc_tested, bc_positive)
+
+combined_data <- rbind(strataa_blood_culture_data, mitima_data)
+
 # Create time periods
-blood_culture_data$year <- cut(blood_culture_data$date, 
+combined_data$year <- cut(combined_data$date, 
                                   breaks = "year",
                                   labels = FALSE)
 
-# Calculate blood culture counts by quarter
-bc_by_year <- blood_culture_data %>%
+# Calculate blood culture counts by year
+bc_by_year <- combined_data %>%
   group_by(year) %>%
   summarize(
     n_tested = sum(bc_tested),
@@ -36,9 +76,9 @@ bc_by_year <- blood_culture_data %>%
   )
 
 # Prepare model inputs
-n_years <- length(unique(blood_culture_data$year))
+n_years <- length(unique(combined_data$year))
 n_BCpos <- aggregate(bc_positive ~ year, 
-                     data = blood_culture_data, 
+                     data = combined_data, 
                      FUN = sum)$bc_positive
 n_tested <- bc_by_year$n_tested
 n_total <- bc_by_year$n_total
@@ -47,7 +87,8 @@ alpha.bc <- n_tested
 beta.bc <- n_total - n_tested
 
 # Person-time at risk
-persontime <- rep(100000, n_years)
+#persontime <- rep(100000, n_years)
+persontime <- c(100000,100000,100000,58333,100000,100000)
 
 # Parameters for blood culture sensitivity
 mu_sensitivity <- 0.59
